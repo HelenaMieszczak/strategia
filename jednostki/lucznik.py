f9b270690e3af3f7d90d13ed1cee887fda6f2a0f
HelenaMieszczak <helenamieszczak@eu.pl>
2019-06-27 19:03:55 +0200

    update

--- jednostki/lucznik.py
+++ jednostki/lucznik.py
@@ -4,9 +4,20 @@ from wojownik import Wojownik
 class Lucznik(Wojownik):
     def __init__(self):
         super().__init__(10)
+        self.strzaly = 15
 
-    def atakuj(self):
-        print('Łucznik: Wypuściłem strzałę!')
-        self.doswiadczenie += 0.4
+    def atakuj(self, strzaly):
+        strzelanie = True
+        while strzelanie:
+            print('Łucznik: Wypuściłem strzałę!')
+            self.doswiadczenie += 0.4
+            self.strzaly = self.strzaly - 1
+            return self.doswiadczenie
+            if self.strzaly == 0:
+                print("Już nie postrzelasz")
+                strzelanie = False
+
+
+
 
 
